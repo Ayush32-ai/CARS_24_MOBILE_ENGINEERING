409104ef4560e8cb29b64523b6f31332cc9f9e45
Ayush <ayush@example.com>
2026-08-05 11:45:10 +0530

    Fix unresolved reference ContentScale in MainActivity

--- app/src/main/java/com/example/cars24sdui/MainActivity.kt
+++ app/src/main/java/com/example/cars24sdui/MainActivity.kt
@@ -26,6 +26,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.White
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
